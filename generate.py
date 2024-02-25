7ea6c85ee2d8b0ad1d0535efa81b8797f6cfa4bc
Andrew Godwin <andrew@aeracode.org>
2024-02-25 13:18:28 -0700

    Bounded version before I try gosper curves

--- generate.py
+++ generate.py
@@ -1,13 +1,40 @@
 import json
 import sys
 import pyproj
+from shapely.geometry import Point, Polygon
 from hilbertcurve.hilbertcurve import HilbertCurve
 
 # Grid specification
 
-ORIGIN = [-264900, 6807180]
-SIZE = [600, 900]
-CURVE_LEVEL = 10  # Should be an even number
+CURVE_LEVEL = 11  # Should be an even number
+SITE_BOUNDARY = [
+    [-264611.6552161462605, 6807182.048061979934573],
+    [-264574.089565280301031, 6807271.157280313782394],
+    [-264500.705503123463131, 6807259.80022307485342],
+    [-264421.206102453579661, 6807457.23829506803304],
+    [-264394.123889038572088, 6807560.325430002994835],
+    [-264380.14597243728349, 6807721.945090705528855],
+    [-264381.893212012422737, 6807805.812590313144028],
+    [-264251.723863662860822, 6807773.488658172078431],
+    [-264175.718942143197637, 6807947.338995900936425],
+    [-264399.365607763989829, 6808051.299750623293221],
+    [-264686.786517878121231, 6808080.129203613847494],
+    [-264686.786517878121231, 6808058.28870892431587],
+    [-264671.061361701693386, 6808042.563552747480571],
+    [-264750.560762371576857, 6807833.768423515371978],
+    [-264788.126413237536326, 6807771.74141859728843],
+    [-264774.148496636247728, 6807724.565950067713857],
+    [-264780.263835149351507, 6807672.148762813769281],
+    [-264809.96690792706795, 6807638.951210885308683],
+    [-264841.417220279981848, 6807630.215013009496033],
+    [-264845.785319218004588, 6807614.92666672822088],
+    [-264829.186543253948912, 6807608.374518320895731],
+    [-264825.255254209798295, 6807583.913164269179106],
+    [-264875.488391995721031, 6807498.298425085842609],
+    [-264892.96078774734633, 6807453.743815918453038],
+    [-264895.581647110113408, 6807250.190405412577093],
+    [-264611.6552161462605, 6807182.048061979934573],
+]
 
 # Emoji choice
 
@@ -99,6 +126,8 @@ EMOJI_CATEGORIES = [
     "keycap",
     "alphanum",
 ]
+
+# Extract individual emoji based on categories
 with open("all-emoji.json") as fh:
     all_emojis = json.load(fh)
 emojis = []
@@ -115,25 +144,43 @@ for category in EMOJI_CATEGORIES:
     if not found:
         raise ValueError(f"Bad category {category}")
 
-print(emojis)
-
-
-# Validate the number of emoji
-curve = HilbertCurve(CURVE_LEVEL, 2)
-hilbert_points = int((2**CURVE_LEVEL) - (1 / (2**CURVE_LEVEL))) + 1
-assert (
-    len(emojis) >= hilbert_points
-), f"Not enough emojis (have {len(emojis)}, need {hilbert_points}"
-print(f"{hilbert_points} points", file=sys.stderr)
-print(f"{len(emojis)} emojis", file=sys.stderr)
-
-# Find the maximum curve dimension the dumb way (I forget the equation)
-max_x = 0
-max_y = 0
-for i in range(hilbert_points):
-    x, y = curve.point_from_distance(i)
+# Calculate the site origin and bounds
+min_x = SITE_BOUNDARY[0][0]
+min_y = SITE_BOUNDARY[0][1]
+max_x = SITE_BOUNDARY[0][0]
+max_y = SITE_BOUNDARY[0][1]
+for x, y in SITE_BOUNDARY[1:]:
+    min_x = min(min_x, x)
+    min_y = min(min_y, y)
     max_x = max(max_x, x)
     max_y = max(max_y, y)
+size_x = max_x - min_x
+size_y = max_y - min_y
+
+# Find the maximum curve dimension the dumb way (I forget the equation)
+curve = HilbertCurve(CURVE_LEVEL, 2)
+hilbert_points = int((2**CURVE_LEVEL) - (1 / (2**CURVE_LEVEL))) + 1
+max_curve_x = 0
+max_curve_y = 0
+for i in range(hilbert_points):
+    x, y = curve.point_from_distance(i)
+    max_curve_x = max(max_curve_x, x)
+    max_curve_y = max(max_curve_y, y)
+
+# Go through and generate a list of all curve points within the boundary
+boundary_polygon = Polygon(SITE_BOUNDARY)
+valid_points = []
+for i in range(hilbert_points):
+    cx, cy = curve.point_from_distance(i)
+    x = min_x + ((cx / max_curve_x) * size_x)
+    y = min_y + ((cy / max_curve_y) * size_y)
+    if boundary_polygon.contains(Point(x, y)):
+        valid_points.append((x, y))
+
+# Validate the number of emoji
+print(f"{len(valid_points)} valid points")
+print(f"{len(emojis)} emojis")
+assert len(emojis) >= len(valid_points), "Not enough emojis!"
 
 # Prep coordinate systems
 latlong = pyproj.CRS.from_epsg(4326)
@@ -142,12 +189,8 @@ merc_to_lat = pyproj.Transformer.from_crs(sphmerc, latlong)
 
 # Generate GeoJSON
 features = []
-for i in range(hilbert_points):
-    x, y = curve.point_from_distance(i)
-    lat, long = merc_to_lat.transform(
-        ORIGIN[0] + ((x / max_x) * SIZE[0]),
-        ORIGIN[1] + ((y / max_y) * SIZE[1]),
-    )
+for i, (x, y) in enumerate(valid_points):
+    lat, long = merc_to_lat.transform(x, y)
     features.append(
         {
             "type": "Feature",
